a7e490b8b6810a1c330249478dce8376fb8e22b4
calalumajr <calalumajr@gmail.com>
2023-08-22 15:39:28 -0700

    mstk

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# A puppet manuscript intended to fix a bug in wp-setings.php
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
